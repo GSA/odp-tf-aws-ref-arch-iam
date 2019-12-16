fab6bae15773c1c481f56a29e29008d7cb6a72c3
RobertJLupinek <9854828608@GSA.GOV>
2019-12-16 17:32:00 -0500

    (feature) Adding grace IAM Policy, Roles, Groups, and Users.

--- example/variables.tf
+++ example/variables.tf
@@ -3,3 +3,18 @@ variable "aws_region" {
   description = "Default region used by some modules"
   default = "us-east-1"
 }
+
+variable "env" {
+  description = "AWS region to launch servers."
+  default     = "sandbox"
+}
+
+variable "aws_account_id" {
+  description = "aws account ID"
+  default = "xxxxxx"
+}
+
+variable "ip_whitelist" {
+  description = "source IP whitelist"
+  default = "10.1.1.0/24"
+}
